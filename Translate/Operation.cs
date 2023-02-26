f708a2a6a0117edde50f20ec34183d9dc1590f6f
Isa Memmedli <i.a.memmedli@gmail.com>
2023-02-27 03:21:59 +0400

    v1.1.1 Added Log

--- Translate/Operation.cs
+++ Translate/Operation.cs
@@ -1,153 +1,174 @@
 ﻿using Google.Cloud.Translation.V2;
+using Serilog;
 using System.Diagnostics;
 using System.Text;
 
-namespace Translate
+namespace isaaholic.POtranslate.Core;
+
+// 1.1.1
+public static class Operation
 {
-    // 1.1.0
-    // 1.1.1
-    public static class Operation
+    private static StreamReader? stream;
+    private static List<string> lines = new List<string>();
+    private static TranslationClient client; // Google Cloud API Key
+    private static bool isOpen = false;
+    private static string newFilePath;
+
+    public static string Path { get; private set; } // path of Folder
+    public static string FileName { get; private set; } // without .po
+    public static int FileLineCount { get; private set; } // count of lines of file
+
+    static Operation()
     {
-        private static StreamReader? stream;
-        private static List<string> lines = new List<string>();
-        public static TranslationClient client { private get; set; } // Google Cloud API Key
-        private static bool isOpen = false;
-        private static string newFilePath;
-
-        public static string Path { get; private set; } // path of Folder
-        public static string FileName { get; private set; } // without .po
-        public static int FileLineCount { get; private set; } // count of lines of file
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo
+            .File(Directory.GetCurrentDirectory() + "\\Logs\\poLog.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+    }
 
 
-
-        public static void OpenFile(string fileName, string folderPath)
+    public static void OpenFile(string fileName, string folderPath)
+    {
+        try
         {
-            try
-            {
-                stream = new StreamReader(folderPath + "\\" + fileName + ".po");
-                FileLineCount = File.ReadAllLines(folderPath + "\\" + fileName + ".po").Length;
-                Path = folderPath;
-                FileName = fileName;
-                isOpen = true;
-                newFilePath = $"{Path}\\{FileName}_new.po";
-            }
-            catch (Exception)
-            {
-                // Add Log
-            }
+            stream = new StreamReader(folderPath + "\\" + fileName + ".po");
+            FileLineCount = File.ReadAllLines(folderPath + "\\" + fileName + ".po").Length;
+            Path = folderPath;
+            FileName = fileName;
+            isOpen = true;
+            newFilePath = $"{Path}\\{FileName}_new.po";
         }
-
-        public static void CreateClientFromApiKey(string apiKey)
+        catch (Exception ex)
         {
-            client = TranslationClient.CreateFromApiKey(apiKey);
-            // Add Log
+            Log.Error(ex.Message);
+            isOpen = false;
         }
+    }
 
-        private static void DeleteNewFile()
+    public static void CreateClientFromApiKey(string apiKey)
+    {
+        client = TranslationClient.CreateFromApiKey(apiKey);
+        Log.Information("API Key added.");
+        try
         {
-            if (File.Exists(newFilePath))
-            {
-                File.Delete(newFilePath);
-            }
-            using var sw = new StreamWriter(newFilePath);
+            client.TranslateText("Testing", LanguageCodes.English, LanguageCodes.Azerbaijani);
         }
-
-        public static async void Translate()
+        catch (Exception ex)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            if (isOpen)
-            {
-                List<string> unTranslatedLines = new();
-                DeleteNewFile();
+            Log.Error(ex.Message, "error_Log");
+            isOpen = false;
+        }
+    }
 
-                while (true)
+    private static void DeleteNewFile()
+    {
+        if (File.Exists(newFilePath))
+        {
+            File.Delete(newFilePath);
+            Log.Warning($"{newFilePath} was removed.");
+        }
+        using var sw = new StreamWriter(newFilePath);
+        Log.Information($"{newFilePath} was added");
+    }
+
+    public static async void Translate()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        if (isOpen)
+        {
+            Log.Information("Translate has started");
+            List<string> unTranslatedLines = new();
+            DeleteNewFile();
+
+            while (true)
+            {
+                var line = stream.ReadLine();
+
+                if (line == null)
+                    break;
+
+                StringBuilder poLine = new();
+                StringBuilder unTranslatedLine = new();
+                TranslationResult? response;
+                string detectedLine;
+
+                if (line.Length > 5 && line.Remove(5, line.Length - 5) == "msgid")
                 {
-                    var line = stream.ReadLine();
-
-                    if (line == null)
-                        break;
-
-                    StringBuilder poLine = new();
-                    StringBuilder unTranslatedLine = new();
-                    TranslationResult? response;
-                    string detectedLine;
-
-                    if (line.Length > 5 && line.Remove(5, line.Length - 5) == "msgid")
+                    int count = 5;
+                    if (line[5] == '_')
+                        count = 12;
+                    else unTranslatedLines.Clear();
+                    lines.Add(line);
+                    foreach (var character in line.Remove(0, count))
                     {
-                        int count = 5;
-                        if (line[5] == '_')
-                            count = 12;
-                        else unTranslatedLines.Clear();
-                        lines.Add(line);
-                        foreach (var character in line.Remove(0, count))
-                        {
-                            unTranslatedLine.Append(character);
-                        }
-                        unTranslatedLines.Add(unTranslatedLine.ToString());
+                        unTranslatedLine.Append(character);
                     }
-                    else if (line.Length > 7 && line.Remove(7, line.Length - 7) == "msgstr ")
+                    unTranslatedLines.Add(unTranslatedLine.ToString());
+                }
+                else if (line.Length > 7 && line.Remove(7, line.Length - 7) == "msgstr ")
+                {
+                    poLine.Append("msgstr");
+                    bool once = false;
+                    foreach (var unTranslatedl in unTranslatedLines)
                     {
-                        poLine.Append("msgstr");
-                        bool once = false;
-                        foreach (var unTranslatedl in unTranslatedLines)
+                        if (unTranslatedl[unTranslatedl.Length - 1] == unTranslatedl[unTranslatedl.Length - 2] && unTranslatedl[unTranslatedl.Length - 1] == '"')
                         {
-                            if (unTranslatedl[unTranslatedl.Length - 1] == unTranslatedl[unTranslatedl.Length - 2] && unTranslatedl[unTranslatedl.Length - 1] == '"')
-                            {
-                                response = client.TranslateText(unTranslatedl.ToString().Remove(unTranslatedl.Length - 1, 1), LanguageCodes.Azerbaijani, LanguageCodes.English);
-                                poLine.Append(response.TranslatedText.Replace('“', '"').Replace('”', '"') + '"');
-                            }
-                            else
-                            {
-                                response = client.TranslateText(unTranslatedl.ToString(), LanguageCodes.Azerbaijani, LanguageCodes.English);
-                                poLine.Append(response.TranslatedText.Replace('“', '"').Replace('”', '"'));
-                            }
-                            if (unTranslatedLines[unTranslatedLines.Count - 1] != unTranslatedl)
-                                poLine.AppendLine();
-                            if (once)
-                            {
-                                stream.ReadLine();
-                            }
-                            else
-                                once = true;
-                        }
-                        unTranslatedLines.Clear();
-                        lines.Add(poLine.ToString());
-                    }
-                    else if (line.Length > 7 && line.Remove(7, line.Length - 7) == "msgstr[")
-                    {
-                        int index = int.Parse(line[7].ToString());
-                        poLine.Append("msgstr[" + index + ']');
-
-                        if (unTranslatedLines[index][unTranslatedLines[index].Length - 1] == unTranslatedLines[index][unTranslatedLines[index].Length - 2] && unTranslatedLines[index][unTranslatedLines[index].Length - 1] == '"')
-                        {
-                            response = client.TranslateText(unTranslatedLines[index].ToString().Remove(unTranslatedLines[index].Length - 1, 1), LanguageCodes.Azerbaijani, LanguageCodes.English);
+                            response = client.TranslateText(unTranslatedl.ToString().Remove(unTranslatedl.Length - 1, 1), LanguageCodes.Azerbaijani, LanguageCodes.English);
                             poLine.Append(response.TranslatedText.Replace('“', '"').Replace('”', '"') + '"');
                         }
                         else
                         {
-                            response = client.TranslateText(unTranslatedLines[index].ToString(), LanguageCodes.Azerbaijani, LanguageCodes.English);
+                            response = client.TranslateText(unTranslatedl.ToString(), LanguageCodes.Azerbaijani, LanguageCodes.English);
                             poLine.Append(response.TranslatedText.Replace('“', '"').Replace('”', '"'));
                         }
-                        lines.Add(poLine.ToString());
+                        if (unTranslatedLines[unTranslatedLines.Count - 1] != unTranslatedl)
+                            poLine.AppendLine();
+                        if (once)
+                        {
+                            stream.ReadLine();
+                        }
+                        else
+                            once = true;
                     }
-                    else if (line.Length != 0 && line[0] == '"')
+                    unTranslatedLines.Clear();
+                    lines.Add(poLine.ToString());
+                }
+                else if (line.Length > 7 && line.Remove(7, line.Length - 7) == "msgstr[")
+                {
+                    int index = int.Parse(line[7].ToString());
+                    poLine.Append("msgstr[" + index + ']');
+
+                    if (unTranslatedLines[index][unTranslatedLines[index].Length - 1] == unTranslatedLines[index][unTranslatedLines[index].Length - 2] && unTranslatedLines[index][unTranslatedLines[index].Length - 1] == '"')
                     {
-                        lines.Add(line);
-                        unTranslatedLines.Add(line);
+                        response = client.TranslateText(unTranslatedLines[index].ToString().Remove(unTranslatedLines[index].Length - 1, 1), LanguageCodes.Azerbaijani, LanguageCodes.English);
+                        poLine.Append(response.TranslatedText.Replace('“', '"').Replace('”', '"') + '"');
                     }
                     else
                     {
-                        lines.Add(line);
-                        unTranslatedLines.Clear();
+                        response = client.TranslateText(unTranslatedLines[index].ToString(), LanguageCodes.Azerbaijani, LanguageCodes.English);
+                        poLine.Append(response.TranslatedText.Replace('“', '"').Replace('”', '"'));
                     }
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    //Console.WriteLine($"Progress - {lines.Count}/{FileLineCount}"); // AddLog
+                    lines.Add(poLine.ToString());
                 }
-                await File.WriteAllLinesAsync(newFilePath, lines);
+                else if (line.Length != 0 && line[0] == '"')
+                {
+                    lines.Add(line);
+                    unTranslatedLines.Add(line);
+                }
+                else
+                {
+                    lines.Add(line);
+                    unTranslatedLines.Clear();
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                //Console.WriteLine($"Progress - {lines.Count}/{FileLineCount}"); // AddLog
+                Log.Information($"Progress - {lines.Count}/{FileLineCount}");
             }
-            stopwatch.Stop();
-            // Add Log
+            await File.WriteAllLinesAsync(newFilePath, lines);
+            Log.Information("Translate was finished");
         }
+        stopwatch.Stop();
+        Log.Information("Time Elapsed " + stopwatch.Elapsed.ToString());
     }
 }
